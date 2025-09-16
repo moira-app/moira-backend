524f55a2fa2f2675b8aef5ed5578757691c9a64a
BJW <backj123@naver.com>
2025-09-17 01:39:31 +0900

    [BJW] Chat 패키지 생성 및 ensureRoom 테스트 코드 작성

---
+++ src/main/java/com/org/server/chat/domain/ChatMessage.java
@@ -0,0 +1,58 @@
+package com.org.server.chat.domain;
+
+import com.org.server.util.BaseTime;
+import com.sun.jdi.CharType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class ChatMessage extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	// 채팅 타입 ( PROJECT, MEET )
+	@Enumerated(EnumType.STRING)
+	private ChatType chatType;
+
+	// 방 Id ( 프로젝트Id, 미트Id )
+	private Long roomId;
+
+	// 사용자Id
+	private Long senderId;
+
+	// 내용
+	private String content;
+
+
+	@Builder
+	private ChatMessage(ChatType chatType, Long roomId, Long senderId, String content) {
+		this.chatType = chatType;
+		this.roomId = roomId;
+		this.senderId = senderId;
+		this.content = content;
+	}
+
+	public static ChatMessage of(ChatType chatType, long roomId, long senderId, String content) {
+		return ChatMessage.builder()
+			.chatType(chatType)
+			.roomId(roomId)
+			.senderId(senderId)
+			.content(content)
+			.build();
+	}
+
+
+}
