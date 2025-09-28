75deb3f587b06181459f208e031fe2fb4124edd0
BJW <backj123@naver.com>
2025-09-29 00:47:51 +0900

    [BJW] 채팅방/메시지 API 구현 및 테스트 페이지 추가

--- src/main/java/com/org/server/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/org/server/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,9 @@
 package com.org.server.chat.repository;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +14,9 @@ public interface ChatRoomRepository  extends JpaRepository<ChatRoom, Long> {
 
 	Optional<ChatRoom> findByChatTypeAndRefId(ChatType chatType, Long refId);
 
+	Optional<ChatRoom> findByRefId(Long refId);
+
+	List<ChatRoom> findByRefIdIs(Long refId);
+
+	Optional<ChatRoom> findByid(Long id);
 }
