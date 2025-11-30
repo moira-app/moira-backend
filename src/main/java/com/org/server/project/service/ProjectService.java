9dfbaabfbb714f3852b5a3cd48e84cbe807b4601
dong3058 <hwangdonggeun70@gmail.com>
2025-11-30 19:23:44 +0900

    make project ticket for project creator

--- src/main/java/com/org/server/project/service/ProjectService.java
+++ src/main/java/com/org/server/project/service/ProjectService.java
@@ -3,8 +3,12 @@ package com.org.server.project.service;
 
 
 import com.org.server.exception.MoiraException;
+import com.org.server.member.domain.Member;
+import com.org.server.member.service.SecurityMemberReadService;
 import com.org.server.project.domain.Project;
 import com.org.server.project.repository.ProjectRepository;
+import com.org.server.ticket.domain.Ticket;
+import com.org.server.ticket.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,7 +24,8 @@ import java.util.UUID;
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
-
+    private final SecurityMemberReadService securityMemberReadService;
+    private final TicketService ticketService;
 
     public Boolean checkProject(Long id){
         Optional<Project> p=projectRepository.findById(id);
@@ -33,6 +38,13 @@ public class ProjectService {
         UUID url=UUID.randomUUID();
         Project project=new Project(title,url.toString());
         project=projectRepository.save(project);
+        Member member=securityMemberReadService.securityMemberRead();
+        Ticket ticket=Ticket.builder()
+                .projectId(project.getId())
+                .memberId(member.getId())
+                .alias(member.getNickName())
+                .build();
+        ticketService.saveTicket(ticket);
         return url.toString();
     }
     public void delProject(Long id){
